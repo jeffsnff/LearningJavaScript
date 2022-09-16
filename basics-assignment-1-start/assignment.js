418fc0cb1c05cbf1c13563f1992de5fd42698638
Jeffrey Seneff <jeffsnff@gmail.com>
2022-09-16 09:25:10 -0400

    First assignment completed

---
+++ basics-assignment-1-start/assignment.js
@@ -0,0 +1,13 @@
+let userInput = 9;
+let result;
+
+result = 18 + userInput;
+
+// subtract
+result = result - 2;
+// multiply
+result = result * 2;
+// divide
+result = result / 2
+
+alert(`The result = ${result} and the user input = ${userInput}`)
