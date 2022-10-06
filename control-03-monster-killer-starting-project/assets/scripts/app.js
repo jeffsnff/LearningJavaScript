b2446c2b5eceebe053adec1aa52249a55fdb6f23
Jeffrey Seneff <jeffsnff@gmail.com>
2022-10-06 18:02:27 -0400

    reformated the code to make better

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -18,72 +18,76 @@ function attackHandler(mode) {
   attack('ATTACK');
 }
 
-function monsterAttack(){
-  const monsterAttackDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
-  playerHealth -= monsterAttackDamage;
-  addLog(`Monster Attacked for ${monsterAttackDamage}`)
-}
-
-function playerAttack(maxDamage){
-  const playerAttackDamage = dealMonsterDamage(maxDamage);
-  monsterHealth -= playerAttackDamage;
-}
-
-function winOrLose(){
-  if (monsterHealth <= 0) {
-    alert('The Monster has been vanquished!');
-    addLog(`Player has succeeded`)
-  } else if (playerHealth <= 0) {
-    alert('You have been vanquished!');
-    addLog(`Player has died`)
-  }
-}
-
-function attack(mode) {
-  let maxDamage
-  if (mode === 'ATTACK') {
-    maxDamage = ATTACK_VALUE;
-    addLog(`Player ATTACKED for ${maxDamage}`)
-  } else if (mode === 'STRONG_ATTACK') {
-    let critical = Math.floor(Math.random(10)* 10)
-    maxDamage = ATTACK_VALUE + critical;
-    addLog(`Player STRONG ATTACKED for ${maxDamage}`)
-  }
-
-  if (playerHealth > 0 && monsterHealth > 0) {
-    // const playerAttackDamage = dealMonsterDamage(maxDamage);
-    // monsterHealth -= playerAttackDamage;
-    playerAttack(maxDamage)
-    monsterAttack()
-  }
-  winOrLose()
-}
-
 function strongAttackHandler() {
   attack('STRONG_ATTACK');
 }
 
-function healPlayer() {
+function monsterAttack() {
+  const monsterAttackDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  playerHealth -= monsterAttackDamage;
+  addLog(`Monster Attacked for ${monsterAttackDamage}`);
+}
 
-  if(playerHealth >= chosenMaxLife){
-    console.log("You are at max health!")
-    return
+function playerAttack(maxDamage) {
+  const playerAttackDamage = dealMonsterDamage(maxDamage);
+  monsterHealth -= playerAttackDamage;
+}
+
+function winOrLose() {
+  if (monsterHealth <= 0) {
+    alert('The Monster has been vanquished!');
+    addLog(`Player has succeeded`);
+  } else if (playerHealth <= 0) {
+    alert('You have been vanquished!');
+    addLog(`Player has died`);
+  }
+}
+
+function attack(mode) {
+  let maxDamage;
+
+  // determines which attack value to use normal or strong
+  if (mode === 'ATTACK') {
+    maxDamage = ATTACK_VALUE;
+    addLog(`Player ATTACKED for ${maxDamage}`);
+  } else if (mode === 'STRONG_ATTACK') {
+    let critical = Math.floor(Math.random(10) * 10);
+    maxDamage = ATTACK_VALUE + critical;
+    addLog(`Player STRONG ATTACKED for ${maxDamage}`);
+  }
+
+  // only runs if both monster and player are alive
+  if (playerHealth > 0 && monsterHealth > 0) {
+    playerAttack(maxDamage);
+    monsterAttack();
+  }
+  winOrLose();
+}
+
+function healPlayer() {
+  console.log(playerHealth)
+  if (playerHealth <= 0 || monsterHealth <= 0) {
+    return;
+  }
+  if (playerHealth >= chosenMaxLife) {
+    console.log('You are at max health!');
+    return;
   }
   restoreHealth = Math.floor(Math.random(10) * 10);
   // increases player health bar
   increasePlayerHealth(restoreHealth);
   // increases player health
   playerHealth += restoreHealth;
-  
-  addLog(`Player Healed : ${restoreHealth}`)
-  monsterAttack()
-  winOrLose()
+
+  addLog(`Player Healed : ${restoreHealth}`);
+  monsterAttack();
+  winOrLose();
 }
 
 function showLog() {
-  console.log(logs)
+  console.log(logs);
 }
 
-function addLog(log){
-  logs.push(log)
+function addLog(log) {
+  logs.push(log);
 }
